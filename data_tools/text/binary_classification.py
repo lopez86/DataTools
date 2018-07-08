573419f3a40ddeb5e9eaf5ced8ea8dbf41c8a65e
Jeremy Lopez <jeremy.p.lopez@gmail.com>
2018-07-08 14:36:54 -0400

    Fixing some scope stuff.

--- data_tools/text/binary_classification.py
+++ data_tools/text/binary_classification.py
@@ -27,16 +27,16 @@ def binary_text_classification_cnn(
                 name_scope=scope, var_scope=scope
             )
             max_pools.append(global_max_pool(activations, scope))
-        concatenated = concatenate(max_pools, name_scope='dense')
+        concatenated = concatenate(max_pools, name_scope='concat')
         dropped_out = dropout(
-            concatenated, is_train, name_scope='dense', keep_prob=do_keep_frac
+            concatenated, is_train, name_scope='dropout', keep_prob=do_keep_frac
         )
         activations = dense_layer(
             dropped_out, dense_size,
             name_scope='dense', variable_scope='dense'
         )
         output = dense_layer(
-            activations, 1, name_scope='output', variable_scope='output',
+            activations, 1, name_scope='out_dense', variable_scope='out_dense',
             activation=tf.nn.sigmoid
         )
         with tf.name_scope('output'):
